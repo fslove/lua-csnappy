8af6e1b1f5428d7f71448bc0cfc792d00e807b88
Francois Perrad <francois.perrad@gadz.org>
2012-10-18 15:50:08 +0200

    manages bad cases

--- lsnappy.c
+++ lsnappy.c
@@ -8,39 +8,72 @@
 #include "csnappy/csnappy_compress.c"
 #include "csnappy/csnappy_decompress.c"
 
+
 static int lsnappy_compress(lua_State *L)
 {
     void* ud;
     size_t src_len;
-    size_t dest_len;
     const char* src = luaL_checklstring(L, 1, &src_len);
     size_t dest_max = csnappy_max_compressed_length(src_len);
     lua_Alloc alloc = lua_getallocf(L, &ud);
     char* working_memory = alloc(ud, NULL, 0, CSNAPPY_WORKMEM_BYTES);
-    char* dest = alloc(ud, NULL, 0, dest_max);
-    csnappy_compress(src, src_len, dest, &dest_len, working_memory,
-                     CSNAPPY_WORKMEM_BYTES_POWER_OF_TWO);
-    lua_pushlstring(L, dest, dest_len);
-    /* freeing */
-    (void)alloc(ud, dest, dest_max, 0);
-    (void)alloc(ud, working_memory, CSNAPPY_WORKMEM_BYTES, 0);
-    return 1;
+    if (working_memory != NULL) {
+        char* dest = alloc(ud, NULL, 0, dest_max);
+        if (dest != NULL) {
+            size_t dest_len;
+            csnappy_compress(src, src_len, dest, &dest_len, working_memory, CSNAPPY_WORKMEM_BYTES_POWER_OF_TWO);
+            lua_pushlstring(L, dest, dest_len);
+            /* freeing */
+            (void)alloc(ud, dest, dest_max, 0);
+            (void)alloc(ud, working_memory, CSNAPPY_WORKMEM_BYTES, 0);
+            return 1;
+        }
+        else {
+            (void)alloc(ud, working_memory, CSNAPPY_WORKMEM_BYTES, 0);
+        }
+    }
+    return luaL_error(L, "snappy: not enough memory");
 }
 
 static int lsnappy_decompress(lua_State *L)
 {
-    void* ud;
     size_t src_len;
     size_t dest_len;
     const char* src = luaL_checklstring(L, 1, &src_len);
-    size_t header_len = csnappy_get_uncompressed_length(src, src_len, &dest_len);
-    lua_Alloc alloc = lua_getallocf(L, &ud);
-    char* dest = alloc(ud, NULL, 0, dest_len);
-    csnappy_decompress_noheader(src + header_len, src_len - header_len, dest, &dest_len);
-    lua_pushlstring(L, dest, dest_len);
-    /* freeing */
-    (void)alloc(ud, dest, dest_len, 0);
-    return 1;
+    int header_len = csnappy_get_uncompressed_length(src, src_len, &dest_len);
+    if (header_len != CSNAPPY_E_HEADER_BAD) {
+        if (dest_len == 0) {
+            lua_pushlstring(L, "", 0);
+        }
+        else {
+            void* ud;
+            lua_Alloc alloc = lua_getallocf(L, &ud);
+            char* dest = alloc(ud, NULL, 0, dest_len);
+            if (dest != NULL) {
+                int ret = csnappy_decompress_noheader(src + header_len, src_len - header_len, dest, &dest_len);
+                if (ret == CSNAPPY_E_OK) {
+                    lua_pushlstring(L, dest, dest_len);
+                    /* freeing */
+                    (void)alloc(ud, dest, dest_len, 0);
+                }
+                else {
+                    switch (ret) {
+                      case CSNAPPY_E_DATA_MALFORMED:
+                        return luaL_error(L, "snappy: malformed data");
+                      case CSNAPPY_E_OUTPUT_OVERRUN:
+                        return luaL_error(L, "snappy: output overrun");
+                      default:
+                        return luaL_error(L, "snappy: error (%d)", ret);
+                    }
+                }
+            }
+            else {
+                return luaL_error(L, "snappy: not enough memory");
+            }
+        }
+        return 1;
+    }
+    return luaL_error(L, "snappy: bad header");
 }
 
 
--- test/01-snappy.t
+++ test/01-snappy.t
@@ -2,9 +2,25 @@
 
 require 'Test.More'
 
-plan(1025)
+plan(4 + 1025)
 
 local snappy = require 'snappy'
+
+error_like(function () snappy.compress(snappy) end,
+           "bad argument #1 to 'compress' %(string expected, got table%)",
+           "bad argument")
+
+error_like(function () snappy.decompress(snappy) end,
+           "bad argument #1 to 'decompress' %(string expected, got table%)",
+           "bad argument")
+
+error_like(function () snappy.decompress '' end,
+           "snappy: bad header",
+           "bad header")
+
+error_like(function () snappy.decompress 'malformed' end,
+           "snappy: malformed data",
+           "malformed data")
 
 for i = 0, 1024 do
     local input = string.rep('0', i)
