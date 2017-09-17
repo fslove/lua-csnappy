016a208b55bae07fa688979166b0b5cf2c8c78e9
Francois Perrad <francois.perrad@gadz.org>
2017-09-17 09:23:14 +0200

    refactor with lua_newuserdata

--- lsnappy.c
+++ lsnappy.c
@@ -11,28 +11,15 @@
 
 static int lsnappy_compress(lua_State *L)
 {
-    void* ud;
     size_t src_len;
     const char* src = luaL_checklstring(L, 1, &src_len);
     size_t dest_max = csnappy_max_compressed_length(src_len);
-    lua_Alloc alloc = lua_getallocf(L, &ud);
-    char* working_memory = alloc(ud, NULL, 0, CSNAPPY_WORKMEM_BYTES);
-    if (working_memory != NULL) {
-        char* dest = alloc(ud, NULL, 0, dest_max);
-        if (dest != NULL) {
-            uint32_t dest_len;
-            csnappy_compress(src, src_len, dest, &dest_len, working_memory, CSNAPPY_WORKMEM_BYTES_POWER_OF_TWO);
-            lua_pushlstring(L, dest, dest_len);
-            /* freeing */
-            (void)alloc(ud, dest, dest_max, 0);
-            (void)alloc(ud, working_memory, CSNAPPY_WORKMEM_BYTES, 0);
-            return 1;
-        }
-        else {
-            (void)alloc(ud, working_memory, CSNAPPY_WORKMEM_BYTES, 0);
-        }
-    }
-    return luaL_error(L, "snappy: not enough memory");
+    char* working_memory = lua_newuserdata(L, CSNAPPY_WORKMEM_BYTES);
+    char* dest = lua_newuserdata(L, dest_max);
+    uint32_t dest_len;
+    csnappy_compress(src, src_len, dest, &dest_len, working_memory, CSNAPPY_WORKMEM_BYTES_POWER_OF_TWO);
+    lua_pushlstring(L, dest, dest_len);
+    return 1;
 }
 
 static int lsnappy_decompress(lua_State *L)
@@ -47,27 +34,18 @@ static int lsnappy_decompress(lua_State *L)
             return 1;
         }
         else {
-            void* ud;
-            lua_Alloc alloc = lua_getallocf(L, &ud);
-            char* dest = alloc(ud, NULL, 0, dest_len);
-            if (dest != NULL) {
-                int ret = csnappy_decompress_noheader(src + header_len, src_len - header_len, dest, &dest_len);
-                switch (ret) {
-                    case CSNAPPY_E_OK:
-                        lua_pushlstring(L, dest, dest_len);
-                        /* freeing */
-                        (void)alloc(ud, dest, dest_len, 0);
-                        return 1;
-                    case CSNAPPY_E_DATA_MALFORMED:
-                        return luaL_error(L, "snappy: malformed data");
-                    case CSNAPPY_E_OUTPUT_OVERRUN:
-                        return luaL_error(L, "snappy: output overrun");
-                    default:
-                        return luaL_error(L, "snappy: error (%d)", ret);
-                }
-            }
-            else {
-                return luaL_error(L, "snappy: not enough memory");
+            char* dest = lua_newuserdata(L, dest_len);
+            int ret = csnappy_decompress_noheader(src + header_len, src_len - header_len, dest, &dest_len);
+            switch (ret) {
+                case CSNAPPY_E_OK:
+                    lua_pushlstring(L, dest, dest_len);
+                    return 1;
+                case CSNAPPY_E_DATA_MALFORMED:
+                    return luaL_error(L, "snappy: malformed data");
+                case CSNAPPY_E_OUTPUT_OVERRUN:
+                    return luaL_error(L, "snappy: output overrun");
+                default:
+                    return luaL_error(L, "snappy: error (%d)", ret);
             }
         }
     }
